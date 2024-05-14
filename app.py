b5bdab8b3c64c55c77d33967588b7af05a8c7bac
styven <styvenn16@gmail.com>
2024-05-14 05:54:31 -0300

    rotas de login e logout em flask

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "senhasuperdificil"
 #ligação com o banco de dados
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+login_manager = LoginManager()
 db = SQLAlchemy(app)
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 CORS(app)
+
+
+
 #///////
 #banco de dados
 #tabela de usuario
@@ -26,8 +33,32 @@ class Product(db.Model):
 
 
 #rotas
+#autenticação
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+##########################################################################################
+
+#login no sistema
+@app.route('/login/',methods=["POST"])
+def login():
+    data = request.json
+    
+    user = User.query.filter_by(username=data.get("username")).first()
+    if user and data.get("password") == user.password:
+            login_user(user)
+            return jsonify({"message":"Entrou no sistema"})
+    return jsonify({"message":"Senha ou usuario incorretos"})  
+##########################################################################################
+#sair do sistema
+@app.route('/logout/', methods=["POST"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message":"saiu no sistema"})  
 #cadastro de produtos
 @app.route('/api/products/add', methods=["POST"])
+@login_required
 def add_product():
     data = request.json
     if 'name' in data and 'price' in data:
@@ -37,9 +68,11 @@ def add_product():
         return jsonify({"message":"produto cadastrado com sucesso"}), 200   
     else:
         return jsonify({"message":"produto invalido"}), 400
+##########################################################################################
     
 #cadastro de usuarios
 @app.route('/api/user/add', methods=["POST"])
+@login_required
 def add_user():
     data = request.json
     if 'username' in data and 'password' in data:
@@ -49,9 +82,11 @@ def add_user():
         return jsonify({"message":"Usuario cadastrado com sucesso"}), 200   
     else:
         return jsonify({"message":"Erro ao adicionar um usuario"}), 400    
+##########################################################################################
 
 #deletar produtos
 @app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
+@login_required
 def delete_product(product_id):
     product = Product.query.get(product_id)
     if product:
@@ -60,9 +95,8 @@ def delete_product(product_id):
         return jsonify({"message":"produto deletado com sucesso","NOME":product.name,}), 200   
     else:
         return jsonify({"message":"produto não encontrado"}), 400 
-    
-    
-       
+##########################################################################################
+      
 #rota de achar um produto
 @app.route('/api/products/<int:product_id>', methods=["GET"])
 def get_product_details(product_id):
@@ -71,10 +105,11 @@ def get_product_details(product_id):
         return jsonify({"ID":product.id,"NOME":product.name,"PRECO":product.price,"DESCRICAO":product.description}), 200
     else:
         return jsonify({"message": "produto não encontrado"}),400
-    
-    
+##########################################################################################
+   
 #rota de atualização 
 @app.route('/api/products/update/<int:product_id>', methods=["PUT"])
+@login_required
 def update_product(product_id):
     product = Product.query.get(product_id)
     if not product:
@@ -90,7 +125,7 @@ def update_product(product_id):
         product.description = data['description']    
     db.session.commit()
     return jsonify({"messager":"Produto atualizado com sucesso"}), 200
-
+##########################################################################################
 
 #lista dos produtos
 @app.route('/api/products', methods=["GET"])
@@ -101,6 +136,7 @@ def get_product():
         product_data = {"ID":p.id,"NOME":p.name,"PRECO":p.price,"DESCRICAO":p.description}
         product_list.append(product_data)
     return jsonify(product_list)
+##########################################################################################
              
 @app.route('/')
 def hello_world():
@@ -108,3 +144,4 @@ def hello_world():
 
 if __name__ == "__main__":
     app.run(debug=True)    
+##########################################################################################
